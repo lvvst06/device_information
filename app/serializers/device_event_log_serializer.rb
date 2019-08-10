f040f6363f96c4aba3478d19b62ea34480acc855
Vuong Lam <vuonglv@ipcoin.vn>
2019-08-10 20:56:18 +0700

    remove serializers

--- app/serializers/device_event_log_serializer.rb
+++
@@ -1,7 +0,0 @@
- class DeviceEventLogSerializer < ActiveModel::Serializer
-  attributes :id, :event_name, :timestamp, :created_at_timestamp
-
-  def created_at_timestamp
-    object.created_at.to_s
-  end
-end
