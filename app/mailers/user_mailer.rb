52be22b9236a69f2fefd258012dcbf3767d2a018
Vuong Lam <vuonglv@ipcoin.vn>
2019-08-10 21:57:00 +0700

    Loggers

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,6 @@
+class UserMailer < ApplicationMailer
+  def loggers(text)
+    @text = text
+    mail(to: 'admin@device_information.com', subject: 'Loggers')
+  end
+end
---
+++ app/services/loggers.rb
@@ -0,0 +1,29 @@
+class Loggers
+  def initialize(text, mode = :console)
+    @text = text
+    @mode = mode
+  end
+
+  def execute
+    case @mode
+    when :console
+      p @text}
+    when :file
+      write_file
+    when :mail
+      send_email
+    end
+  end
+
+  private
+
+  def write_file
+    file = File.open('log/loggers.txt', 'w')
+    file.puts @text
+    file.close
+  end
+
+  def send_email
+    UserMailer.delay.loggers(@text)
+  end
+end
