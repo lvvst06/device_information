65da6a36ce5b7b3409a05b17d5a8753f8d583ae1
Vuong Lam <vuonglv@ipcoin.vn>
2019-08-11 08:25:13 +0700

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :event_logs, :controller => 'device_event_logs', only: [:create]
+    resources :event_logs, controller: 'device_event_logs', only: [:create]
   end
 end
